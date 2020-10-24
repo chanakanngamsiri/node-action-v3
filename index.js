d3b27582782c81c911d4d44b7666fb6f5b7f644d
Chanakan Ngamsiri <60160032@go.buu.ac.th>
2020-10-24 16:07:44 +0700

    long test lasets

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
+let items = [{id:1,name:'iphone 12 pro max'},{id:2,name:'Googel Pixel 5'}]
+
 app.get('/', (req, res) => {
     return res.send('Hello!')
+})
+
+app.get('/items', (req, res) => {
+    return res.json(items)
 })
 
 app.listen(port, () => {
--- tests/test.js
+++ tests/test.js
@@ -2,6 +2,7 @@ const assert = require('assert')
 const request = require('supertest')
 const app = require('../index')
 
+
 describe('Validate setting the test', () => {
     it('should be ok', () => {
         assert.ok(true)
@@ -11,5 +12,13 @@ describe('Validate setting the test', () => {
 describe('GET "/"', () => {
     it('respond with Hello!', (done) => {
         request(app).get('/').expect('Hello!',done)
+    })
+
+})
+
+describe('GET "/items"', () => {
+    it('respond with /items', (done) => {
+        let want= '[{"id":1,"name":"iphone 12 pro max"},{"id":2,"name":"Googel Pixel 5"}]'
+        request(app).get('/items').expect(want,done)
     })
 })
